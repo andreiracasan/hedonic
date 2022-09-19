4b05526577a9e84a8e42657ebced4d40123eeb42
Andrei Racasan <69733376+0xAMR@users.noreply.github.com>
2022-09-19 21:53:41 +0100

    ✨  create navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,19 @@
+// Styles
+import styles from './navbar.module.scss';
+
+export default function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.nav__links}>
+        <h1>Hedonic</h1>
+        <span>|</span>
+        <h3>top</h3>
+        <h3>new</h3>
+      </div>
+
+      <div className={styles.nav__pages}>
+        <h3>about</h3>
+      </div>
+    </nav>
+  );
+}
