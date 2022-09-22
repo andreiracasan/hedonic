d78fcd572ec08bf47db48bcb8519b61cb98438b5
Andrei Racasan <69733376+0xAMR@users.noreply.github.com>
2022-09-22 18:11:10 +0100

    ♻️ simplify nav

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,15 +5,13 @@ export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.nav__links}>
-        <h1>Hedonic</h1>
-        <span>|</span>
+        <h1>Hedonic for HN</h1>
+        {/* <span>|</span>
         <h3>top</h3>
-        <h3>new</h3>
+        <h3>new</h3> */}
       </div>
 
-      <div className={styles.nav__pages}>
-        <h3>about</h3>
-      </div>
+      <div className={styles.nav__pages}>{/* <h3>about</h3> */}</div>
     </nav>
   );
 }
