34b70dbfcbeeb7e24ceacf44cf2eb67b9590430d
Andrei Racasan <69733376+0xAMR@users.noreply.github.com>
2022-09-23 20:45:51 +0100

    💄 minor tweaks

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,20 @@
 // Styles
 import styles from './navbar.module.scss';
 
+// Next
+import { useRouter } from 'next/router';
+
 export default function Navbar() {
+  const router = useRouter();
+
   return (
     <nav className={styles.nav}>
       <div className={styles.nav__links}>
-        <h1>Hedonic for HN</h1>
-        {/* <span>|</span>
-        <h3>top</h3>
-        <h3>new</h3> */}
+        <h1>Hedonic</h1>
+        <span>|</span>
+        <button onClick={() => router.push('/top/1')}>TOP</button>
+        <button onClick={() => router.push('/new/1')}>NEW</button>
       </div>
-
       <div className={styles.nav__pages}>{/* <h3>about</h3> */}</div>
     </nav>
   );
