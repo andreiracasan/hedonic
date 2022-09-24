d5d5b644e2f825c746f0808b23737c6d9e47ea46
Andrei Racasan <69733376+0xAMR@users.noreply.github.com>
2022-09-24 10:13:08 +0100

    ✨ add links to dynamic routes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import styles from './navbar.module.scss';
 
 // Next
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function Navbar() {
   const router = useRouter();
@@ -10,10 +11,24 @@ export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.nav__links}>
-        <h1>Hedonic</h1>
+        <Link href="/">
+          <a className={styles.nav__links_home}>
+            <h1>Hedonic</h1>
+          </a>
+        </Link>
         <span>|</span>
-        <button onClick={() => router.push('/top/1')}>TOP</button>
-        <button onClick={() => router.push('/new/1')}>NEW</button>
+        <Link href="/top/1">
+          <a>Top</a>
+        </Link>
+        <Link href="/new/1">
+          <a>New</a>
+        </Link>
+        <Link href="/show/1">
+          <a>Show</a>
+        </Link>
+        <Link href="/ask/1">
+          <a>Ask</a>
+        </Link>
       </div>
       <div className={styles.nav__pages}>{/* <h3>about</h3> */}</div>
     </nav>
