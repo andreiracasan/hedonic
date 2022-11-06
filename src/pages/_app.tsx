da90b4a70fa8b7a1696166bd564485a6c0a771f7
Andrei Racasan <69733376+0xAMR@users.noreply.github.com>
2022-11-06 18:00:25 +0000

    ✨ add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
-import '../../styles/globals.css';
+// Next
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+// Styling
+import '../../styles/globals.css';
 
 // Components
 import Navbar from '../components/Navbar';
@@ -8,6 +12,9 @@ import Footer from '../components/Footer';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Hedonic App</title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
